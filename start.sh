197d62eff81d180b7650bba5d8652fd19447ff23
Paul Lu <sininglu@sandiego.edu>
2023-08-21 14:42:04 +0800

    reduced print statement for debugging

--- start.sh
+++ start.sh
@@ -3,10 +3,7 @@
 set -e 
 
 echo "Run db migrations"
-ls -la /app
 source /app/app.env
-
-echo "DB_SOURCE: $DB_SOURCE"
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
 echo "Run app"
