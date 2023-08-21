e2c2458644fadb3ccc288642ec83ad1695063bd1
Paul Lu <sininglu@sandiego.edu>
2023-08-21 14:19:15 +0800

    added

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 echo "Run db migrations"
 ls -la /app
 source /app/app.env
+cat /app/app.env
 echo "DB_SOURCE: $DB_SOURCE"
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
